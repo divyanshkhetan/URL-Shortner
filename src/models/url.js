f04def9b465694fccd9ac0a3952293d14cca8670
Divyansh Khetan <divyanshkhetan18@gmail.com>
2024-02-05 23:50:46 +0530

    database

---
+++ src/models/url.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const urlSchema = new mongoose.Schema({
+    originalUrl: {
+        type: String,
+        required: true,
+    },
+    code: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Url = mongoose.model('Url', urlSchema);
+
+export default Url;
